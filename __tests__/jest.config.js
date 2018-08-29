c532e6475953d85f2a427945477effa71ceb0f4d
Nate <ndsunc@gmail.com>
2018-08-29 13:45:05 -0700

    Updated Jest config w/ coverageDirectory path

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   setupTestFrameworkScriptFile: './support.js', // setup before each test run
   verbose: true,
-  coverageDirectory: '../coverage'
+  coverageDirectory: './../coverage'
 }
