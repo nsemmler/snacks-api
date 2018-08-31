bfa83fa7bfe19d8bf147adcfade446534d12ffe7
Nate <ndsunc@gmail.com>
2018-08-31 10:32:22 -0700

    Added remaining snacksController methods; show is only uncommented for now

--- controllers/snacks.js
+++ controllers/snacks.js
@@ -17,6 +17,49 @@ function index(req, res, next) {
 		.catch(err => next(err))
 }
 
+async function show(req, res, next) {
+	let data
+	snack.getSnackById(req.params.id)
+		.then(found => data = found)
+		.then(() => review.getSnackReviews(req.params.id))
+		.then(reviews => data.reviews = reviews)
+		.then(() => res.status(201).json({ data }))
+		.catch(err => next(err))
+}
+
+// function featured(req, res, next) {
+// 	snack.getFeatured()
+// 		.then(data => res.status(201).json({ data }))
+// 		.catch(err => next(err))
+// }
+//
+// function create(req, res, next) {
+// 	isValidSnackCreate(req.body)
+// 		.then(() => snack.create(req.body))
+// 		.then(data => res.status(201).json({ data }))
+// 		.catch(err => next(err))
+// }
+//
+// function update(req, res, next) {
+// 	isValidSnackPatch(req.body)
+// 		.then(() => snack.getSnackById(req.params.id))
+// 		.then(() => snack.update(req.params.id, req.body))
+// 		.then(data => res.status(200).json({ data }))
+// 		.catch(err => next(err))
+// }
+//
+// function destroy(req, res, next) {
+//   	snack.getSnackById(req.params.id)
+// 	  	.then(() => snack.update(req.params.id, req.body))
+// 		.then(data => res.status(202).json({ data }))
+// 		.catch(err => next(err))
+// }
+
 module.exports = {
-  index
+	index,
+	// show,
+	// featured,
+	// create,
+	// update,
+	// destroy
 }
