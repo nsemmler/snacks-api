712f37b25ec3e47e1be69cc4dc58e498f10e7309
Nate <ndsunc@gmail.com>
2018-09-04 18:02:33 -0700

    Moved snacksController.featured() route above the routes ending in /:id

--- routes/snacks.js
+++ routes/snacks.js
@@ -5,10 +5,10 @@ const { snacksController } = require('../controllers')
 router.get('/', snacksController.index)
 router.post('/', snacksController.create)
 
+router.get('/featured', snacksController.featured)
+
 router.get('/:id', snacksController.show)
 router.patch('/:id', snacksController.update)
 router.delete('/:id', snacksController.destroy)
-
-router.get('/featured', snacksController.featured)
 
 module.exports = router
